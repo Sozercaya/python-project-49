0eb25da7d6733895d1b3816d3c89d858d1d51c63
Светлана Борисенкова <borisenkova_1995@mail.ru>
2026-02-19 17:12:56 +0300

    add discription

---
+++ brain_games/games/calc.py
@@ -0,0 +1,21 @@
+from random import randint, choice
+
+DESCRIPTION = 'What is the result of the expression?'
+
+
+def generate_round():
+    num1 = randint(1, 50)
+    num2 = randint(1, 50)
+    operations = ['+', '-', '*']
+    operation = choice(operations)
+
+    question = f'{num1} {operation} {num2}'
+
+    if operation == '+':
+        correct_answer = num1 + num2
+    elif operation == '-':
+        correct_answer = num1 - num2
+    elif operation == '*':
+        correct_answer = num1 * num2
+    
+    return question, str(correct_answer)
